cc52cc92a6f3ce08b3a4f56e4bdadddabe774bbf
Ankur Saini <sainiankur0508@gmail.com>
2024-12-04 10:57:53 +0530

    Update Auth.js

--- backend/Middlewares/Auth.js
+++ backend/Middlewares/Auth.js
@@ -1,22 +1,25 @@
 const jwt = require('jsonwebtoken');
-const { mkcol } = require('../Routes/AuthRouter');
+
 const ensureAuthenticated = (req, res, next) => {
     const auth = req.headers[ 'authorization' ];
     if (!auth) {
         return res.status(403)
-        .json({ message: 'Unauthorized, JWT token is require in auth'})
+        .json({ message: 'Unauthorized, JWT token is require'})
     }
+    const token = auth.split(' ')[1];
     try{
-        console.log("is working in auth");
-        const decoded = jwt.verify(auth, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
 
     }
     catch (err) {
+        console.log("No welcome")
         return res.status(403)
-        .json({ message: 'Unauthorized, JWT token is wrong or expired in auth'})
+        .json({ message: 'Unauthorized, JWT token is wrong or expired'})
     }
 }
+
+console.log("auth is working");
 
 module.exports = ensureAuthenticated;

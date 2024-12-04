5c2d54b3771e6807908207dcfd3e92fd27a0a107
Ankur Saini <sainiankur0508@gmail.com>
2024-12-04 11:03:32 +0530

    Update Signup.js

--- mi-1/src/component/Signup.js
+++ mi-1/src/component/Signup.js
@@ -17,7 +17,7 @@ function Signup() {
   const navigate = useNavigate();
   const handleChange = (e) => {
     const { name, value } = e.target;
-    console.log("hello")
+    
     const copySignupInfo = {...signupInfo};
     copySignupInfo[name] = value;
     setSignupInfo(copySignupInfo)
@@ -25,18 +25,18 @@ function Signup() {
 
   const handleSubmit = async(e) => {
     e.preventDefault();
-     const {name, email, password, company, department, designation} = signupInfo;
-    console.log("hello1")
+    console.log("working")
+    const {name, email, password, company, department, designation} = signupInfo;
+   
+    console.log(signupInfo)
     if(!name || !email || !password){
       return handleError('Name, Email, or, password are required')
     }
     if(password !== confirmPassword){
       return handleError('password and confirm Password are not equil')
     }
-    console.log("hello2")
     try {
-      const url = "https://deploy-mi-test-api.vercel.app/auth/signup";
-      console.log("hello3")
+      const url = "http://localhost:8080/auth/signup";
       const response = await fetch(url, {
         method: "POST",
         headers: {
@@ -44,40 +44,34 @@ function Signup() {
         },
         body: JSON.stringify(signupInfo)
       })
-      console.log("hello4")
       const result = await response.json();
       const {success, message, error} = result;
-      console.log("hello5")
       if(success){
-        console.log("hello6")
         handleSuccess(message);
         setTimeout(()=>{
           navigate('/login')
         },500)
       }else if(error){
-        console.log("hello7")
           const details = error?.details[0].message;
           handleError(details)
       }else if(!success){
-        console.log("hello8")
         handleError(message)
       }
       console.log(result)
     }
     catch(err) {
-      console.log("hello9")
       handleError(err);
     }
   };
 
   return (
     <div className="d-flex justify-content-center align-items-center vh-100">
-       <div className="card shadow-3d p-4" style={{position: 'absolute', top: '25vh', width: '100%', maxWidth: '400px',  backgroundColor: 'white', borderRadius: '10px', boxShadow: '0 20px 40px rgba(0, 0, 0, 0.2), 0 10px 15px rgba(0, 0, 0, 0.1)',}}>
+      <div className="card shadow-3d p-4" style={{position: 'absolute', top: '25vh', width: '100%', maxWidth: '400px',  backgroundColor: 'white', borderRadius: '10px', boxShadow: '0 20px 40px rgba(0, 0, 0, 0.2), 0 10px 15px rgba(0, 0, 0, 0.1)',}}>
         <h1 className="text-center mb-4">Signup</h1>
         <form onSubmit={handleSubmit}>
           <fieldset>
             <div className="mb-3">
-              <label htmlFor="name" className="form-label" style={{fontWeight: 'bold'}}>Name</label>
+              <label htmlFor="name" className="form-label" style={{fontWeight: 'bold'}}>Name*</label>
               <input
                 type="text"
                 name="name"
@@ -126,9 +120,9 @@ function Signup() {
               />
             </div>
             <div className="mb-3">
-              <label htmlFor="email" className="form-label" style={{fontWeight: 'bold'}}>Email</label>
+              <label htmlFor="email" className="form-label" style={{fontWeight: 'bold'}}>Email*</label>
               <input
-                type="email"
+                type="text"
                 name="email"
                 id="email"
                 className="form-control"
@@ -138,7 +132,7 @@ function Signup() {
               />
             </div>
             <div className="mb-3">
-              <label htmlFor="password" className="form-label" style={{fontWeight: 'bold'}}>Password</label>
+              <label htmlFor="password" className="form-label" style={{fontWeight: 'bold'}}>Password*</label>
               <input
                 type="password"
                 name="password"
@@ -174,3 +168,4 @@ function Signup() {
 }
 
 export default Signup;
+

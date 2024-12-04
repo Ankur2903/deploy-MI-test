497f12a509608f7e340a35f243b73f6e928dbd30
Ankur Saini <sainiankur0508@gmail.com>
2024-12-04 10:57:27 +0530

    Update AuthController.js

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -4,27 +4,24 @@ const UserModel = require('../Models/User')
 
 const signup = async (req, res) => {
     try {
-        const { name, email, password, company, department, designation} = req.body;
+        const { name, email, password, company, department, designation, manager} = req.body;
         const user = await UserModel.findOne({ email });
         if (user) {
-            console.log("signup working 1 in login in authcontrolle");
             return res.status(409)
-            .json({ message: 'User is already exist, you can login in login in authcontrolle' , success: false } ) ;
+            .json({ message: 'User is already exist, you can login' , success: false } ) ;
         }
-        const userModel = new UserModel({name, email, password, company, department, designation}) ;
+        const userModel = new UserModel({name, email, password, company, department, designation, manager}) ;
         userModel.password = await bcrypt.hash(password, 10);
         await userModel.save();
         res.status(201)
         .json({
-            message: "Signup successfully ",
+            message: "You can signup after approvel",
             success: true
         })
-        console.log("signup working 2 in login in authcontrolle");
     } catch (err){
-        console.log("signup working 3 in login in authcontrolle");
-        res.status(0)
+        res.status(500)
         .json({
-            message: "Internal server error in signup AuthController",
+            message: "Internal server error in AuthController>>signup",
             success: false
         })
     }
@@ -32,39 +29,42 @@ const signup = async (req, res) => {
 
 const login = async (req, res) => {
     try {
-        const { name, email, password } = req.body;
+        const {email, password } = req.body;
         const user = await UserModel.findOne({ email });
         if (!user) {
-            console.log("login working 1 in login in authcontrolle");
             return res.status(403)
             .json({ message: 'Auth failed email or password is wrong' , success: false } ) ;
         }
-        console.log("login working 2");
        const isPassEqual = await bcrypt.compare(password, user.password)
        if(!isPassEqual){
-           console.log("login working 3");
             return res.status(403)
-            .json({ message: 'Auth failed email or password is wrong in login in authcontrolle' , success: false } ) ;
+            .json({ message: 'Auth failed email or password is wrong' , success: false } ) ;
        }
-        console.log("login working 4");
-       const jwtToke = jwt.sign(
+       if(user.status === "pending"){
+            return res.status(403)
+            .json({ message: 'You can Login after Approvel' , success: false } ) ;
+       }
+       if(user.status === "rejected"){
+            return res.status(403)
+            .json({ message: "You can't login your requeast is rejected" , success: false } ) ;
+       }
+       const jwtToken = jwt.sign(
         { email: user.email, _id: user.id },
         process.env.JWT_SECRET,
         { expiresIn: '24h'}
        )
         res.status(200)
         .json({
-            message: "Login successfully in login in authcontroller",
+            message: "Signup successfully ",
             success: true,
-            jwtToke,
+            jwtToken,
             email,
             name: user.name
         })
     } catch (err){
-        console.log("login working 1 in login in authcontrolle");
-        res.status(0)
+        res.status(500)
         .json({
-            message: "Internal server error in login in authcontroller",
+            message: "Internal server error",
             success: false
         })
     }

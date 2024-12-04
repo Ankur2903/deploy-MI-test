f20270f6316f40f3f350af3b02ed7e5614d18f19
Ankur Saini <sainiankur0508@gmail.com>
2024-12-04 11:10:23 +0530

    Update RefrshHandler.js

--- mi-1/src/RefrshHandler.js
+++ mi-1/src/RefrshHandler.js
@@ -1,17 +1,18 @@
 import React, { useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 
-function RefrshHandler({ setlsAuthenticated } ) {
+function RefrshHandler({ setlsAuthenticated, setLoading } ) {
     const location = useLocation()
     const navigate = useNavigate();
     
     useEffect(() => {
-        if (localStorage.getItem( 'token')) {
+        if (localStorage.getItem('token')) {
             setlsAuthenticated(true);
             if (location.pathname === '/login' || location.pathname === '/signup'){
                 navigate('/',{ replace: false });
             }
         }
+        setLoading(false);
     },[location, navigate, setlsAuthenticated])
     return(
         null

8901748c46bc92ee56d2f9995b2fb38cc83058e6
Ankur Saini <sainiankur0508@gmail.com>
2024-12-07 16:42:00 +0530

    Update Navbar.js

--- mi-1/src/component/Navbar.js
+++ mi-1/src/component/Navbar.js
@@ -3,15 +3,20 @@ import { Link, useNavigate, useLocation } from 'react-router-dom';
 import logo from './Image/logo.192.jpg';
 import { handleSuccess } from '../ulits';
 import { ToastContainer } from 'react-toastify';
+import CollectData from '../CollectData';
 
 function Navbar() {
   const location = useLocation();
   const currentPath = location.pathname;
   const navigate = useNavigate();
   const [loggedInUser, setLoggedInUser] = useState('');
+  const [users, setUsers] = useState([]);
+  const [permission,setPermission] = useState(false)
   useEffect(() => {
+    console.log("permission", permission)
     setLoggedInUser(localStorage.getItem('loggedInUser'))
   },[])
+  
 
   const handleLogout = (e) => {
     localStorage.removeItem('token');
@@ -24,13 +29,13 @@ function Navbar() {
 
   return (
     <>
-    
+      <CollectData setUsers = {setUsers} setPermission = {setPermission}/>
       <nav className="navbar navbar-white bg-white">
         <div className="container-fluid d-flex align-items-center justify-content-between flex-lg-row flex-column text-lg-start text-center">
           {/* Links Section */}
           <div className="d-flex align-items-center mb-2 mb-lg-0">
             {currentPath!=='/login' && currentPath!=='/signup' && <Link className="nav-link active me-3" aria-current="page" to="/" style={{fontWeight: "bold"}}>Home</Link>}
-            {currentPath!=='/login' && currentPath!=='/signup' && <Link className="nav-link active me-3" aria-current="page" to="/manager_deshboard" style={{fontWeight: "bold"}}>Deshboard</Link>}
+            {currentPath!=='/login' && currentPath!=='/signup' && permission && <Link className="nav-link active me-3" aria-current="page" to="/manager_deshboard" style={{fontWeight: "bold"}}>Deshboard</Link>}
             <Link className="nav-link active me-3" aria-current="page" to="/" onClick={(e) => {e.preventDefault();navigate(-1);}}  style={{fontWeight: "bold"}}>Back</Link>
            
             {currentPath!=='/login' && currentPath!=='/signup' && <button className="nav-link active me-3" onClick={handleLogout}  style={{fontWeight: "bold"}}>Logout</button>}
@@ -67,4 +72,5 @@ function Navbar() {
 }
 
 export default Navbar;
+
 

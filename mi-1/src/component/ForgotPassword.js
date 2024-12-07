bb8ef8c750aea001550f347b0ad3705d25f5b21b
Ankur Saini <sainiankur0508@gmail.com>
2024-12-07 17:24:17 +0530

    Update ForgotPassword.js

--- mi-1/src/component/ForgotPassword.js
+++ mi-1/src/component/ForgotPassword.js
@@ -13,6 +13,7 @@ function ForgotPassword() {
           const url = "https://deploy-mi-test-api.vercel.app/forgot-password";
           const response = await fetch(url, {
             method: "POST",
+              mode: 'no-cors',
             headers: {
               'Content-Type' : 'application/json'
             },

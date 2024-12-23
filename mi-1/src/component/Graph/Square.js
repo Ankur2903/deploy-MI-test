1a6dc3ad2d036cf0ef8d1526517d14358d437033
Ankur Saini <sainiankur0508@gmail.com>
2024-12-23 15:41:02 +0530

    Update Square.js

--- mi-1/src/component/Graph/Square.js
+++ mi-1/src/component/Graph/Square.js
@@ -141,7 +141,7 @@ function Square_graph({ side1, thickness1, outerRadius1}) {
     <div style={{ position: 'relative' }}>
       <div className="form-check form-switch">
             <input className="form-check-input" onClick={clickOndimensioning} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-            <label className="form-check-label" for="flexSwitchCheckDefault">DIMENSIONING FUNCTION</label>
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">DIMENSIONING FUNCTION</label>
           </div>
       <svg
         viewBox={viewBox}
@@ -201,3 +201,4 @@ function Square_graph({ side1, thickness1, outerRadius1}) {
 }
 
 export default Square_graph;
+

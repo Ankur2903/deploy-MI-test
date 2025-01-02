4fa431395fd9c8daa977212cd649f423d5b0bc20
Ankur Saini <sainiankur0508@gmail.com>
2025-01-02 11:18:57 +0530

    Update ManagerDeshboard.js

--- mi-1/src/component/ManagerDeshboard.js
+++ mi-1/src/component/ManagerDeshboard.js
@@ -61,20 +61,23 @@ const ManagerDashboard = () => {
 
 
   return (
-    <div>
+    <div style={{transform: 'translateY(-30px)'}}>
       <CollectData setUsers = {setUsers}/>
-      <h2 style={{textAlign: "center"}}>Pending Users</h2>
+      <h2 style={{textAlign: "center"}}>User Details</h2>
       <table border="1" style={{ borderCollapse: "collapse", width: "100%" }}></table>
       <thead>
         <tr>
-          <th style={{ width: "10vw", border: "1px solid black", backgroundColor: 'black', color: 'white',textAlign: "center"}}>Name</th>
-          <th style={{ width: "18vw", border: "1px solid black", backgroundColor: 'black', color: 'white',textAlign: "center"}}>Email</th>
-          <th style={{ width: "20vw", border: "1px solid black", backgroundColor: 'black', color: 'white',textAlign: "center"}}>Company</th>
-          <th style={{ width: "20vw", border: "1px solid black", backgroundColor: 'black', color: 'white',textAlign: "center"}}>Department</th>
-          <th style={{ width: "18vw", border: "1px solid black", backgroundColor: 'black', color: 'white',textAlign: "center"}}>Designation</th>
-          <th style={{ width: "6vw", border: "1px solid black", backgroundColor: 'black', color: 'white',textAlign: "center"}}>Status</th>
-          <th style={{ width: "4vw", border: "1px solid black", backgroundColor: 'black', color: 'white',textAlign: "center"}}>Approve</th>
-          <th style={{ width: "4vw", border: "1px solid black", backgroundColor: 'black', color: 'white',textAlign: "center"}}>Reject</th>
+          <th style={{ width: "12vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center"}}>Name</th>
+          <th style={{ width: "16vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center"}}>Email</th>
+          <th style={{ width: "20vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center"}}>Company</th>
+          <th style={{ width: "10vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center"}}>Department</th>
+          <th style={{ width: "10vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center"}}>Designation</th>
+          <th style={{ width: "8vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center"}}>Mobile No.</th>
+          <th style={{ width: "6vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center"}}>User Type</th>
+          <th style={{ width: "4vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center"}}>Change User Type</th>
+          <th style={{ width: "6vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center"}}>Status</th>
+          <th style={{ width: "4vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center"}}>Change Status</th>
+          <th style={{ width: "4vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center"}}>Remove</th>
         </tr>
       </thead>
       <tbody>
@@ -85,9 +88,12 @@ const ManagerDashboard = () => {
           <td style={{textAlign: "center", border: "1px solid black" }}>{user.company}</td>
           <td style={{textAlign: "center", border: "1px solid black" }}>{user.department}</td>
           <td style={{textAlign: "center", border: "1px solid black" }}>{user.designation}</td>
+          <td style={{textAlign: "center", border: "1px solid black" }}>{user.phoneNo}</td>
+          <td style={{textAlign: "center", border: "1px solid black" }}>{user.manager === true ? 'Admin' : 'User'}</td>
+          <td style={{textAlign: "center", border: "1px solid black" }}><button style={{backgroundColor: 'green'}} onClick={() => changeType(user._id)}>Change</button></td>
           <td style={{textAlign: "center", border: "1px solid black" }}>{user.status}</td>
-          <td style={{textAlign: "center", border: "1px solid black" }}><button style={{backgroundColor: 'green'}}  onClick={() => updateUserStatus(user._id, "approved")}>Approve</button></td>
-          <td style={{textAlign: "center", border: "1px solid black" }}><button style={{backgroundColor: 'red'}}  onClick={() => updateUserStatus(user._id, "rejected")}>Reject</button></td>
+          <td style={{textAlign: "center", border: "1px solid black" }}><button style={{backgroundColor: 'green'}} onClick={() => updateUserStatus(user._id)}>Change</button></td>
+          <td style={{textAlign: "center", border: "1px solid black" }}><button style={{backgroundColor: 'red'}} onClick={() => deleteUser(user._id)}>Remove</button></td>
         </tr>
       ))}
       </tbody>
@@ -96,4 +102,3 @@ const ManagerDashboard = () => {
 };
 
 export default ManagerDashboard;
-

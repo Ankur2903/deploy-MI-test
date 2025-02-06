5a2b238736cf2077578756869c02399ffe46830d
Ankur Saini <sainiankur0508@gmail.com>
2025-02-06 11:56:58 +0530

    Update Login.js

--- mi-1/src/component/Login.js
+++ mi-1/src/component/Login.js
@@ -61,7 +61,7 @@ function Login() {
     <div className="d-flex justify-content-center align-items-center vh-99">
     <div style={{backgroundImage: `url(${Image})`,backgroundRepeat: 'repeat-y',backgroundSize: 'contain',width: '100%',height: "700px"}}/>
       <div className="card shadow-3d p-4" style={{position: 'absolute', top: '20vh', width: '100%', maxWidth: '400px', backgroundColor: 'white', borderRadius: '15px', boxShadow: '10 20px 40px rgba(0, 0, 0, 0.2), 0 10px 15px rgba(0, 0, 0, 0.1)'}}>
-         <h2 className="text-center mb-2">MI Profile Generator<text style={{fontSize: "12px"}}>(1.1)</text></h2>
+         <h2 className="text-center mb-2">MI Profile Generator<text style={{fontSize: "12px"}}>(Beta-1)</text></h2>
         <h2 className="text-center mb-4">Login</h2>
         <form onSubmit={handleSubmit}>
           <fieldset>

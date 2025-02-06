f4e62fd67ec55457a3bdf691753192810ba609e6
Ankur Saini <sainiankur0508@gmail.com>
2025-02-06 12:05:13 +0530

    Update FromDxf.js

--- mi-1/src/component/FromDxf.js
+++ mi-1/src/component/FromDxf.js
@@ -4,7 +4,7 @@ import Image from "./Image/background.png"
 const FromDxf = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-6 bg-gray-100" style={{display: "flex",width: '100vw', height: '75vh', textAlign: "center",justifyContent: "center", alignItems: "center", color: "green"}}>
-      <h3>From dxf will update in future</h3>
+      <h3>From dxf feature is under development</h3>
     </div>
   );
 };

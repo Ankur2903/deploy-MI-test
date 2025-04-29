afef74c1e4ad5512551fcce2143e8b1a4919eb82
Ankur Saini <sainiankur0508@gmail.com>
2025-04-29 10:28:39 +0530

    Update Result.js

--- mi-1/src/component/shap/Result.js
+++ mi-1/src/component/shap/Result.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../App.css'
 
 function Result({weightPerLength, length, totalWeight, stripWidth, outLine, area, inertiax, inertiay}) {
+  const [click, setclick] =  useState(false)
+  const handleClick = () => {
+    setclick(click^true)
+  }
   return (
     <>
     <div style={{ fontWeight: 'bold', textAlign: 'center', color: 'white', backgroundColor: '#1b065c'}}>Standard Output</div>
@@ -11,8 +15,9 @@ function Result({weightPerLength, length, totalWeight, stripWidth, outLine, area
         <p className="output-text" ><b>Outline length:</b><p>{outLine} mm</p></p>
         <p className="output-text" ><b>Area of cross section:</b><p>{area} mm^2</p></p>
         <br/>
-    <div style={{ fontWeight: 'bold', textAlign: 'center', color: 'white', backgroundColor: '#1b065c'}}>Advanced Output</div>
-        <p className="output-text" ><b>Center of mass (x):</b><p> 0.00 mm from origin</p></p>
+    <div style={{ fontWeight: 'bold', textAlign: 'center', color: 'white', backgroundColor: '#1b065c'}} onClick={handleClick}>Advanced Output  <i class="fa-solid fa-caret-down px-2"></i></div>
+    { 
+        click && <><p className="output-text" ><b>Center of mass (x):</b><p> 0.00 mm from origin</p></p>
         <p className="output-text" ><b>Center of mass (y):</b><p> 0.00 mm from origin</p></p>
         <p className="output-text" ><b>Moment of inertia I(x):</b><p> {inertiax} cm^4</p></p>
         <p className="output-text" ><b>Moment of resistance W(x):</b><p> ___ cm^3</p></p>
@@ -28,7 +33,8 @@ function Result({weightPerLength, length, totalWeight, stripWidth, outLine, area
         <p className="output-text" ><b>Radius of gyration i(u):</b><p> ___ cm</p></p>
         <p className="output-text" ><b>Moment of inertia I(v):</b><p> ___ cm^4</p></p>
         <p className="output-text" ><b>Moment of resistance W(v):</b><p> ___ cm^3</p></p>
-        <p className="output-text" ><b>Radius of gyration i(v):</b> <p>___ cm</p></p>
+        <p className="output-text" ><b>Radius of gyration i(v):</b> <p>___ cm</p></p></>
+    }
     </>
   )
 }

f33967f82ab1d4ebfd5fe7933b45c9cefa0db287
Ankur Saini <sainiankur0508@gmail.com>
2025-08-29 16:12:12 +0530

    Update ManagerDeshboard.js

--- mi-1/src/component/ManagerDeshboard.js
+++ mi-1/src/component/ManagerDeshboard.js
@@ -4,24 +4,30 @@ import CollectData from "../CollectData";
 const ManagerDashboard = () => {
   const [users, setUsers] = useState([]);
   const [selectedUserId, setSelectedUserId] = useState(null);
-
-  const handleOpenModal = (id) => {
-    setSelectedUserId(id);
+  
+  const addUser = (id) => {
+    if(selectedUsers.includes(id)){
+      setSelectedUsers((prevSelected) => prevSelected.filter((userId) => userId !== id));
+    } else {
+      setSelectedUsers((prevSelected) => [...prevSelected, id]);
+    }
   };
 
-  const updateUserStatus = async (id,status) => {
+  const updateUserStatus = async (selectedUsers ,status) => {
     try {
-      const response = await fetch(`https://deploy-mi-test-api.vercel.app/update-status/${id}`, {
+      const response = await fetch(`https://deploy-mi-test-api.vercel.app/update-status`, {
         method: "PUT", // default method, can be omitted
           headers: {
             "Content-Type": "application/json", // Ensure correct content type
           },
-        body: JSON.stringify({ status: status })
+        body: JSON.stringify({ status: status, selectedUsers : selectedUsers }) // Send status in the request body
         });
       // Update the user list after successful update
-     setUsers((prevUsers) =>
+     for(let i=0;i<selectedUsers.length;i++){
+        const id = selectedUsers[i];
+        setUsers((prevUsers) =>
         prevUsers.map((user) =>
-         user._id === id ? { ...user, status: status} : user
+          user._id === id ? { ...user, status: status} : user
         ));
       const message = await response.json();
     } catch (error) {
@@ -29,33 +35,40 @@ const ManagerDashboard = () => {
     }
   };
 
-  const changeType = async (id) => {
+  const changeType = async (selectedUsers) => {
     try {
-      const response = await fetch(`https://deploy-mi-test-api.vercel.app/change-type/${id}`, {
+      const response = await fetch(`https://deploy-mi-test-api.vercel.app/change-type`, {
         method: "PUT", // default method, can be omitted
           headers: {
             "Content-Type": "application/json", // Ensure correct content type
-          }
+          },
+        body: JSON.stringify({ selectedUsers: selectedUsers })
         });
-      setUsers((prevUsers) =>
-        prevUsers.map((user) =>
-          user._id === id ? { ...user, manager: !user.manager } : user
-        ));
+      for(let i=0;i<selectedUsers.length;i++){
+          const id = selectedUsers[i];
+          setUsers((prevUsers) =>
+          prevUsers.map((user) =>
+            user._id === id ? { ...user, manager: !user.manager } : user
+          ));
       const message = await response.json();
     } catch (error) {
       alert("Failed to change user type");
     }
   }
 
-  const deleteUser = async (id) => {
+  const deleteUser = async (selectedUsers) => {
     try {
-      const response = await fetch(`https://deploy-mi-test-api.vercel.app/delete/${id}`, {
+      const response = await fetch(`https://deploy-mi-test-api.vercel.app/delete`, {
         method: "DELETE", // default method, can be omitted
           headers: {
             "Content-Type": "application/json", // Ensure correct content type
-          }
+          },
+        body: JSON.stringify({ selectedUsers: selectedUsers })
         });
-        setUsers((prevUsers) => prevUsers.filter((user) => user._id !== id));
+        for(let i=0;i<selectedUsers.length;i++){
+          const id = selectedUsers[i];
+          setUsers((prevUsers) => prevUsers.filter((user) => user._id !== id));
+        }
       const message = await response.json();
     } catch (error) {
       alert("Failed to update status");
@@ -66,37 +79,12 @@ const ManagerDashboard = () => {
   return (
     <div>
       <CollectData setUsers = {setUsers}/>
-      <h2 style={{textAlign: "center"}}>User Details</h2>
-      <table border="1" style={{ borderCollapse: "collapse", width: "100%" }}></table>
-      <thead>
-        <tr>
-         <th style={{ width: "10vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>Name</th>
-          <th style={{ width: "14vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>Email</th>
-          <th style={{ width: "16vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>Company</th>
-          <th style={{ width: "11vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>Department</th>
-          <th style={{ width: "11vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>Designation</th>
-          <th style={{ width: "6vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>Mobile No.</th>
-          <th style={{ width: "5vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>User Type</th>
-          <th style={{ width: "4vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>Change User Type</th>
-          <th style={{ width: "4vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>Status</th>
-          <th style={{ width: "4vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>Change Status</th>
-          <th style={{ width: "4vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>Remove</th>
-          <th style={{ width: "5.5vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>Signup Time</th>
-          <th style={{ width: "5.5vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>Last Activity</th>
-        </tr>
-      </thead>
-      <tbody>
-      {users.map((user) => (
-        <tr key={user._id}>
-          <td style={{textAlign: "center", border: "1px solid black" ,fontSize: "13px"}}>{user.name}</td>
-          <td style={{textAlign: "center", border: "1px solid black" ,fontSize: "13px"}}>{user.email}</td>
-          <td style={{textAlign: "center", border: "1px solid black" ,fontSize: "13px"}}>{user.company}</td>
-          <td style={{textAlign: "center", border: "1px solid black" ,fontSize: "13px"}}>{user.department}</td>
-          <td style={{textAlign: "center", border: "1px solid black" ,fontSize: "13px"}}>{user.designation}</td>
-          <td style={{textAlign: "center", border: "1px solid black" ,fontSize: "13px"}}>{user.phoneNo}</td>
-          <td style={{textAlign: "center", border: "1px solid black" ,fontSize: "13px"}}>{user.manager === true ? 'Admin' : 'User'}</td>
-          <td style={{textAlign: "center", border: "1px solid black" ,fontSize: "13px"}}>
-          <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal-1" onClick={() => handleOpenModal(user._id)} style={{backgroundColor: 'green'}}>Change</button>
+      <div  style={{display: "flex", justifyContent: "space-between", alignItems: "center", margin: "5px" }}>
+        {/* Centered Heading */}
+        <h2 style={{ flex: 1, marginLeft: "40vw" }}>User Details</h2>
+        {/* Button Group */}
+        <div className="btn-group" role="group" style={{ display: "flex", gap: "10px", marginLeft: "auto" }}>
+          <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal-1" style={{ backgroundColor: "green", color: "white", padding: "8px 15px", border: "none", borderRadius: "5px" }}>Change User Type</button>
           <div class="modal fade-dark" id="exampleModal-1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
               <div class="modal-content">
@@ -109,15 +97,12 @@ const ManagerDashboard = () => {
                 </div>
                 <div class="modal-footer">
                   <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-primary" data-bs-dismiss="modal"  onClick={() => changeType(selectedUserId)}>Update</button>
+                  <button type="button" class="btn btn-primary" data-bs-dismiss="modal"  onClick={() => changeType(selectedUsers)}>Update</button>
                 </div>
               </div>
             </div>
           </div>
-          </td>
-          <td style={{textAlign: "center", border: "1px solid black" ,fontSize: "13px"}}>{user.status}</td>
-          <td style={{textAlign: "center", border: "1px solid black" ,fontSize: "13px"}}>
-          <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal-2" onClick={() => handleOpenModal(user._id)} style={{backgroundColor: 'green'}}>Change</button>
+          <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal-2" style={{ backgroundColor: "green", color: "white", padding: "8px 15px", border: "none", borderRadius: "5px" }}>Change Status</button>
           <div class="modal fade-dark" id="exampleModal-2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
               <div class="modal-content">
@@ -130,15 +115,13 @@ const ManagerDashboard = () => {
                 </div>
                 <div class="modal-footer">
                   <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-primary" data-bs-dismiss="modal"  onClick={() => updateUserStatus(selectedUserId,"approved")}>Approve</button>
-                  <button type="button" class="btn btn-primary" data-bs-dismiss="modal"  onClick={() => updateUserStatus(selectedUserId,"rejected")}>Reject</button>
+                  <button type="button" class="btn btn-primary" data-bs-dismiss="modal"  onClick={() => updateUserStatus(selectedUsers,"approved")}>Approve</button>
+                  <button type="button" class="btn btn-primary" data-bs-dismiss="modal"  onClick={() => updateUserStatus(selectedUsers,"rejected")}>Reject</button>
                 </div>
               </div>
             </div>
           </div>
-          </td>
-          <td style={{textAlign: "center", border: "1px solid black" ,fontSize: "13px"}}>
-          <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal-3" onClick={() => handleOpenModal(user._id)} style={{backgroundColor: 'red'}}>Remove</button>
+          <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal-3" style={{ backgroundColor: "red", color: "white", padding: "8px 15px", border: "none", borderRadius: "5px" }}>Remove</button>
           <div class="modal fade-dark" id="exampleModal-3" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
               <div class="modal-content">
@@ -151,14 +134,45 @@ const ManagerDashboard = () => {
                 </div>
                 <div class="modal-footer">
                   <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-primary" data-bs-dismiss="modal" onClick={() => deleteUser(selectedUserId)}>Remove</button>
+                  <button type="button" class="btn btn-primary" data-bs-dismiss="modal" onClick={() => deleteUser(selectedUsers)}>Remove</button>
                 </div>
               </div>
             </div>
           </div>
+        </div>
+      </div>
+      <table border="1" style={{ borderCollapse: "collapse", width: "10 0%" }}></table>
+      <thead>
+        <tr>
+          <th style={{ width: "2vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}></th>
+          <th style={{ width: "13vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>Name</th>
+          <th style={{ width: "15vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>Email</th>
+          <th style={{ width: "16vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>Company</th>
+          <th style={{ width: "11vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>Department</th>
+          <th style={{ width: "12vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>Designation</th>
+          <th style={{ width: "7vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>Mobile No.</th>
+          <th style={{ width: "5vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>User Type</th>
+          <th style={{ width: "5vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>Status</th>
+          <th style={{ width: "7vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>Time</th>
+          <th style={{ width: "7vw", border: "1px solid black", backgroundColor: '#1b065c', color: 'white',textAlign: "center",fontSize: "15px"}}>Last Activity</th>
+        </tr>
+      </thead>
+      <tbody>
+      {users.map((user) => (
+        <tr key={user._id}>
+          <td style={{textAlign: "center", border: "1px solid black",fontSize: "13px" }}>
+            <div class="form-check mx-3"><input class="form-check-input" type="checkbox" value="" id="checkDefault" style={{borderRadius: "4px", borderWidth: "2px", borderColor: "black"}} onClick={() => addUser(user._id)}/></div>
           </td>
+          <td style={{textAlign: "center", border: "1px solid black",fontSize: "13px" }}>{user.name}</td>
+          <td style={{textAlign: "center", border: "1px solid black",fontSize: "13px" }}>{user.email}</td>
+          <td style={{textAlign: "center", border: "1px solid black",fontSize: "13px" }}>{user.company}</td>
+          <td style={{textAlign: "center", border: "1px solid black",fontSize: "13px" }}>{user.department}</td>
+          <td style={{textAlign: "center", border: "1px solid black",fontSize: "13px" }}>{user.designation}</td>
+          <td style={{textAlign: "center", border: "1px solid black",fontSize: "13px" }}>{user.phoneNo}</td>
+          <td style={{textAlign: "center", border: "1px solid black",fontSize: "13px" }}>{user.manager === true ? 'Admin' : 'User'}</td>
+          <td style={{textAlign: "center", border: "1px solid black",fontSize: "13px" }}>{user.status}</td>
           <td style={{textAlign: "center", border: "1px solid black",fontSize: "13px" }}>{user.signupTime}</td>
-          <td style={{textAlign: "center", border: "1px solid black" ,fontSize: "13px"}}>{user.lastactivity}</td>
+          <td style={{textAlign: "center", border: "1px solid black",fontSize: "13px" }}>{user.lastactivity}</td>
         </tr>
       ))}
       </tbody>

6371d79cd7dd815c26e27dc33ee956f54dfd2bdc
Ankur Saini <sainiankur0508@gmail.com>
2025-09-15 12:33:19 +0530

    Update AuthController.js

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -74,6 +74,7 @@ const signup = async (req, res) => {
         //     },
         //     saveToSentItems: true
         // };
+        console.log(process.env.USER_EMAIL)
         const response1 = await client.api(`/users/${process.env.USER_EMAIL}/sendMail`).post(email1);
         const response2 = await client.api(`/users/${process.env.USER_EMAIL}/sendMail`).post(email2);
 

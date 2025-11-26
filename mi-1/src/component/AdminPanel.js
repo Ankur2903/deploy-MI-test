6658d5020c1d07019bcebef954e5596b0e0ba3b1
Ankur Saini <sainiankur0508@gmail.com>
2025-11-26 12:37:29 +0530

    Remove unused useEffect import from AdminPanel

--- mi-1/src/component/AdminPanel.js
+++ mi-1/src/component/AdminPanel.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 import ManagerDashboard from "./ManagerDeshboard";
 import Machine from "./Machine";
 import Material from "./Material";

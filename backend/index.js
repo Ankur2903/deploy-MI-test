b6593902a867b76454165bd3ced86664212658d8
Ankur Saini <sainiankur0508@gmail.com>
2025-11-28 11:36:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -109,8 +109,10 @@ app.put("/update-status", ensureAuthenticated, async (req, res) => {
       if (!user) {
         return res.status(404).json({ message: "User not found" });
       }
+         console.log(user);
       user.status = status;
       const updatedUser = await user.save();
+         console.log(updatedUser);
 
         let string;
       if(user.status === 'approved'){
@@ -120,7 +122,7 @@ app.put("/update-status", ensureAuthenticated, async (req, res) => {
         string = `Dear ${user.name}\n\nThank you for signing up for MI Profile Generator.\nWe regret to inform you that your login request has not been approved at this time. If you believe this is an error or require further assistance, please contact us at ${process.env.USER_EMAIL}. \n\nThanks and regards,\nIT Team\nMother India Forming. \n\n************************************************************* The information contained in this message is intended only for use of the individual(s) named above and may contain confidential, proprietary, or legally privileged information. No confidentiality or privilege is waived or lost by any mistransmission. If you are not the intended recipient of this message you are hereby notified that you must not use, disseminate, copy it in any form or take any action in reliance of it. If you have received this message in error, please delete it and any copies of it and notify the sender immediately. ************************************************************`
       }
 
-         console.log(string)
+         console.log(string);
       
       const email1 = {
         message: {
@@ -135,7 +137,7 @@ app.put("/update-status", ensureAuthenticated, async (req, res) => {
         },
         saveToSentItems: true
     };
-         console.log(email1)
+         console.log(email1);
     const response1 = await client.api(`/users/${process.env.USER_EMAIL}/sendMail`).post(email1);
          console.log(response1)
      }     

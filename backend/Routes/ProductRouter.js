0e0d93cac0ae04759d02ab39ce532334a824684d
Ankur Saini <sainiankur0508@gmail.com>
2025-12-23 15:17:08 +0530

    Update ProductRouter.js

--- backend/Routes/ProductRouter.js
+++ backend/Routes/ProductRouter.js
@@ -3,14 +3,6 @@ const ensureAuthenticated = require('../Middlewares/Auth');
 
 <<<<<<< HEAD
 const router = require('express').Router();
-=======
-import {
-  addmaterial,
-  allmaterial,
-  deletematerial,
-  editmaterial,
-} from "../Controllers/materialController.js";
->>>>>>> 1c33eca054996dc181fb3b0d593890fc8646934c
 
 router.post('/addmaterial', ensureAuthenticated, addmaterial)
 router.post('/allmaterials', ensureAuthenticated, allmaterial)

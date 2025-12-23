2d2382a2b581562e94e7fcd9465a751f5ece8e00
Ankur Saini <sainiankur0508@gmail.com>
2025-12-23 15:16:17 +0530

    Update EnquiriRouter.js

--- backend/Routes/EnquiriRouter.js
+++ backend/Routes/EnquiriRouter.js
@@ -1,19 +1,8 @@
 const { addenquirie, allenquiries, deleteenquirie, editenquirie } = require('../Controllers/enquiriesController');
 const ensureAuthenticated = require('../Middlewares/Auth');
 
-<<<<<<< HEAD
 const router = require('express').Router();
 router.post('/addenquirie', ensureAuthenticated, addenquirie);
 router.post('/allenquiries',ensureAuthenticated, allenquiries);
 router.delete('/deleteenquirie',ensureAuthenticated, deleteenquirie)
 router.put('/editenquirie',ensureAuthenticated, editenquirie)
-=======
-import {
-  addenquirie,
-  allenquiries,
-  deleteenquirie,
-  editenquirie,
-} from "../Controllers/enquiriesController.js";
->>>>>>> 1c33eca054996dc181fb3b0d593890fc8646934c
-
-module.exports = router;

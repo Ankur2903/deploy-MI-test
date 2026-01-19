aaceadcb9c70c4f32c6473edb38abfff049ad81c
Ankur Saini <sainiankur0508@gmail.com>
2026-01-19 10:15:25 +0530

    Fix default status value in User model

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -33,7 +33,7 @@ const UserSchema = new Schema({
         required: false,
     },
     status: {
-        type: String, eum: ["pending", "approved", "rejected"], default: "approved"
+        type: String, eum: ["pending", "approved", "rejected"], default: "pending"
     },
     manager: {
         type: String, eum: ["User", "Super User", "Admin"], default: "User"
